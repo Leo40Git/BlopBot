47b996e9f67c2c7e8f04e7b25c426ca300d1d6ed
Leo40Git <7997354+Leo40Git@users.noreply.github.com>
2026-09-06 21:31:42 +0300

    b!gock notes

--- cogs/fun.py
+++ cogs/fun.py
@@ -12,12 +12,15 @@ class Fun(commands.Cog):
     @commands.command(aliases=['pp', 'gock'])
     async def penis(self, ctx: commands.Context):
         """Generates an ASCII penis with a random length for you."""
-        balls_chars = ['8', 'O', 'Ɛ']
+        # TODO seed this for every user
+        # TODO rotate seed every 12 hours (5:30am, 5:30pm)
+
+        balls_chars = ['8', '0', 'Ɛ']
         shaft_chars = ['=', '≈', '≅']
         head_chars = ['D', ')', ']']
 
 
-        length = randrange(3, 12)
+        length = randrange(2, 61)
 
         h = choice(head_chars)
         s = choice(shaft_chars)
