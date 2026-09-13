e5a843ca38dd915c91fa2165b26dd00162c779e0
Leo40Git <7997354+Leo40Git@users.noreply.github.com>
2026-09-13 21:40:05 +0300

    Time-related shenanigans begin

---
+++ cogs/time.py
@@ -0,0 +1,36 @@
+from zoneinfo import ZoneInfo
+
+from discord.ext import commands
+
+from bot import BlopBot
+from utils.context import Context
+from utils.converters import ZoneInfoConverter
+
+
+class Time(commands.Cog):
+    """Commands relating to dates, times, and timezones."""
+
+    @commands.command('settimezone')
+    async def set_timezone(
+            self,
+            ctx: Context,
+            tz: ZoneInfo = commands.parameter(converter=ZoneInfoConverter)):
+        """
+        Sets your timezone.
+
+        This information is used to convert times from your local timezone to BlopBot's timezone
+        when you use them as arguments for commands.
+
+        Parameters
+        ----------
+        ctx
+        tz
+            The timezone to change to.
+        """
+
+        # TODO
+        await ctx.send(f'UH HELLO????? {tz}')
+
+
+async def setup(bot: BlopBot):
+    await bot.add_cog(Time())
---
+++ utils/converters.py
@@ -0,0 +1,18 @@
+import zoneinfo
+from zoneinfo import ZoneInfo
+
+from discord.ext import commands
+
+
+class ZoneInfoConverter(commands.Converter[ZoneInfo]):
+    async def convert(self, ctx: commands.Context, argument: str) -> ZoneInfo:
+        try:
+            return ZoneInfo(argument)
+        except ValueError, zoneinfo.ZoneInfoNotFoundError:
+            raise BadZoneInfoArgument(argument)
+
+
+class BadZoneInfoArgument(commands.BadArgument):
+    def __init__(self, argument: str) -> None:
+        self.argument: str = argument
+        super().__init__(f'"{argument}" is not a recognized time zone.')
