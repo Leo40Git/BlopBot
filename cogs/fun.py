2caf4a77160513d47410c7996c16f861a7da5a31
Leo40Git <7997354+Leo40Git@users.noreply.github.com>
2026-09-13 20:58:54 +0300

    Gock improvements
    Seed penis RNG to user ID and arbitrary "seed" number that is rotated every 12 hours
    Only explicitly reply to !penis message if another message arrived during pause

--- cogs/fun.py
+++ cogs/fun.py
@@ -1,8 +1,8 @@
 import asyncio
-from random import choice, randrange
+from random import Random, choice, randrange, getrandbits
 
 import discord
-from discord.ext import commands
+from discord.ext import tasks, commands
 
 from bot import BlopBot
 from utils.context import Context
@@ -11,31 +11,43 @@ from utils.context import Context
 class Fun(commands.Cog):
     """Various fun commands and games."""
 
-    async def _penis_awe(self, msg: discord.Message):
-        await asyncio.sleep(3)
-        await msg.reply('<:blop:1544849424956792932>')
+    _penis_seed: int
+
+    def __init__(self):
+        self._penis_seed = getrandbits(32)
 
     @commands.command(aliases=['pp', 'gock'])
     async def penis(self, ctx: Context):
         """Generates an ASCII penis with a random length for you."""
-        # TODO seed this for every user
-        # TODO rotate seed every 12 hours (5:30am, 5:30pm)
 
         balls_chars = ['8', '0', 'Ɛ']
         shaft_chars = ['=', '≈', '≅']
         head_chars = ['D', ')', ']']
 
+        rng = Random(hash((ctx.author.id, self._penis_seed)))
 
-        length = randrange(2, 61)
+        length = rng.randrange(2, 61)
 
-        h = choice(head_chars)
-        s = choice(shaft_chars)
-        b = choice(balls_chars)
+        h = rng.choice(head_chars)
+        s = rng.choice(shaft_chars)
+        b = rng.choice(balls_chars)
 
         msg = await ctx.send(b + (s * length) + h)
 
         if length > 7:
             asyncio.create_task(self._penis_awe(msg))
+
+    async def _penis_awe(self, msg: discord.Message):
+        await asyncio.sleep(3)
+
+        if msg.channel.last_message_id == msg.id:
+            await msg.channel.send('<:blop:1544849424956792932>')
+        else:
+            await msg.reply('<:blop:1544849424956792932>')
+
+    @tasks.loop(hours=12)
+    async def _rotate_penis_seed(self):
+        self._penis_seed = getrandbits(32)
 
 
 async def setup(bot: BlopBot):
