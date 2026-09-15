2329d0de03d60aa393a3f0db9be388a931216faf
Leo40Git <7997354+Leo40Git@users.noreply.github.com>
2026-09-15 21:15:04 +0300

    Begin work on !remindme

---
+++ cogs/reminder.py
@@ -0,0 +1,19 @@
+from discord.ext import commands
+
+from bot import BlopBot
+from utils.context import Context
+
+
+class Reminder(commands.Cog):
+    """Commands relating to reminders."""
+
+    @commands.command(aliases=['remind', 'remindme'])
+    def reminder(
+            self,
+            ctx: Context
+    ):
+        pass
+
+
+async def setup(bot: BlopBot):
+    await bot.add_cog(Reminder())
